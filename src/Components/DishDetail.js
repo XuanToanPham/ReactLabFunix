15dd736ef20a48262975e681ce1056571355ab49
PhamToanXuan <phamxuantoan2710@gmail.com>
2022-07-18 10:06:59 +0700

    Fix bug

--- src/Components/DishDetail.js
+++ src/Components/DishDetail.js
@@ -1,20 +1,31 @@
-
 import { COMMENTS } from "../Data/Comment";
 import { data } from "../Data/Data";
 import Comment from "./Comment";
-import {useLocation, useParams} from "react-router-dom"
+import { useLocation, useParams, Link } from "react-router-dom";
+import { Breadcrumb } from "react-bootstrap"; 
 const DishDetail = () => {
   const params = useParams();
-  const location = useLocation()
-  const dish = data.find(dish => dish.id === +params.dishID);
+  const location = useLocation();
+  const dish = data.find((dish) => dish.id === +params.dishID);
   const idComment = +params.dishID;
   const listComment = COMMENTS.filter(
     (comment) => comment.dishId === idComment
   );
-  console.log(listComment)
-  
+  console.log(listComment);
+
   return (
     <div className="row mt-5">
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Link to="/home">Home</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <Link to="/menu">Menu</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>
+          {dish.name}
+        </Breadcrumb.Item>
+      </Breadcrumb>
       <div className="col-6">
         <div className="card">
           <img
@@ -30,7 +41,9 @@ const DishDetail = () => {
       </div>
       <div className="col-6">
         <h3>Comments</h3>
-        {listComment.map(comment => <Comment key = {comment.id} comment={comment}/>)}
+        {listComment.map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
       </div>
     </div>
   );
